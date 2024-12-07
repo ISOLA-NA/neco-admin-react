6cde655937be807f2df1b00d8af268a3545bd2fe
hoseyn7832 <97823900+hasanzade7832@users.noreply.github.com>
2024-12-07 06:01:36 -0800

    refactored  base home page

--- src/components/Views/TabContent.tsx
+++ src/components/Views/TabContent.tsx
@@ -8,20 +8,22 @@ interface TabContentProps {
 
 const TabContent: React.FC<TabContentProps> = ({ ActiveComponent }) => {
   return (
-    <div className="flex-grow bg-white overflow-auto p-6 mt-4 border border-gray-300 rounded-lg mx-4 mb-6">
+    <div className="flex-grow bg-white overflow-hidden mt-4 border border-gray-300 rounded-lg mx-4 mb-6" style={{ height: '100%' }}>
       <React.Suspense fallback={<div>Loading...</div>}>
-        <Splitter style={{ height: '300px' }}>
+        <Splitter style={{ height: '100%' }}>
           <SplitterPanel className="flex align-items-center justify-content-center">
-            <div>
+            <div style={{ margin: '10px' }}>
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
             </div>
           </SplitterPanel>
           <SplitterPanel className="flex align-items-center justify-content-center">
-            {ActiveComponent ? (
-              <ActiveComponent />
-            ) : (
-              <div>Component Not Found</div>
-            )}
+            <div style={{ margin: '10px' }}>
+              {ActiveComponent ? (
+                <ActiveComponent />
+              ) : (
+                <div>Component Not Found</div>
+              )}
+            </div>
           </SplitterPanel>
         </Splitter>
       </React.Suspense>
