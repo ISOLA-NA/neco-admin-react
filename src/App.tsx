cefcdc353f757ca70630faa10ab2b3be8482a586
hoseyn7832 <97823900+hasanzade7832@users.noreply.github.com>
2024-12-21 07:20:04 -0800

    work on test

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,76 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
-} from "react-router-dom";
-import HomePage from "./components/Views/tab/TabbedInterface";
-import Login from "./components/Autentications/Login";
-import Alert from "./components/utilities/Alert/DynamicAlert"; // اطمینان از مسیر صحیح
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-import "leaflet/dist/leaflet.css";
+  Navigate
+} from 'react-router-dom'
+import HomePage from './components/Views/tab/TabbedInterface'
+import Login from './components/Autentications/Login'
+// import Test from './components/Test/test'
+import Alert from './components/utilities/Alert/DynamicAlert'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 
-import "./App.css";
+import './App.css'
 
 const App: React.FC = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
 
   useEffect(() => {
     // بررسی وضعیت احراز هویت از localStorage (اختیاری)
-    const auth = localStorage.getItem("isAuthenticated");
-    if (auth === "true") {
-      setIsAuthenticated(true);
+    const auth = localStorage.getItem('isAuthenticated')
+    if (auth === 'true') {
+      setIsAuthenticated(true)
     }
-  }, []);
+  }, [])
 
   const handleLogin = () => {
-    setIsAuthenticated(true);
-    localStorage.setItem("isAuthenticated", "true"); // ذخیره وضعیت احراز هویت
-  };
+    setIsAuthenticated(true)
+    localStorage.setItem('isAuthenticated', 'true') // ذخیره وضعیت احراز هویت
+  }
 
   const handleLogout = () => {
-    setIsAuthenticated(false);
-    localStorage.removeItem("isAuthenticated");
-  };
+    setIsAuthenticated(false)
+    localStorage.removeItem('isAuthenticated')
+  }
 
   return (
     <Router>
-      <Alert /> {/* ToastContainer در بالاترین سطح اپلیکیشن */}
+      <Alert />
       <Routes>
         <Route
-          path="/"
+          path='/'
           element={
             isAuthenticated ? (
-              // <HomePage />
               <HomePage onLogout={handleLogout} />
             ) : (
-              <Navigate to="/login" replace />
+              <Navigate to='/login' replace />
             )
           }
         />
         <Route
-          path="/login"
+          path='/login'
           element={
             isAuthenticated ? (
-              <Navigate to="/" replace />
+              <Navigate to='/' replace />
             ) : (
               <Login onLogin={handleLogin} />
             )
           }
         />
-        {/* مسیرهای دیگر را در صورت نیاز اضافه کنید */}
+        {/* <Route
+          path='/test'
+          element={
+            <>
+              <Test name="علی" />
+            </>
+          }
+        /> */}
       </Routes>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
