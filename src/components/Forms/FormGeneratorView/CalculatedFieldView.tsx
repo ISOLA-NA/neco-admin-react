70e157a4cbad14b7e31d6bddfc9823b57183d641
hoseyn7832 <97823900+hasanzade7832@users.noreply.github.com>
2025-02-25 03:53:43 -0800

    added map view

--- src/components/Forms/FormGeneratorView/CalculatedFieldView.tsx
+++ src/components/Forms/FormGeneratorView/CalculatedFieldView.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+// src/components/ControllerForms/CalculatedFieldView.tsx
+import React from "react";
+import DynamicInput from "../../utilities/DynamicInput";
 
-function CalculatedFieldView() {
-  return (
-    <div>
-      calculated field
-    </div>
-  )
+interface CalculatedFieldViewProps {
+  data?: {
+    DisplayName?: string;
+  };
 }
 
-export default CalculatedFieldView
+const CalculatedFieldView: React.FC<CalculatedFieldViewProps> = ({ data }) => {
+  return (
+    <div className="p-4">
+      <DynamicInput
+        name={data?.DisplayName || "CalculatedFieldView"}
+        type="text"
+        value=""
+        placeholder=""
+        disabled={true}
+        className="w-full p-2 border rounded focus:outline-none"
+      />
+    </div>
+  );
+};
+
+export default CalculatedFieldView;
--- src/components/Forms/FormGeneratorView/TabView.tsx
+++ src/components/Forms/FormGeneratorView/TabView.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+// src/components/TabView.tsx
+import React from "react";
 
-function TabView() {
-  return (
-    <div>
-      Tab
-    </div>
-  )
+interface TabViewProps {
+  data?: {
+    DisplayName?: string;
+  };
 }
 
-export default TabView
+const TabView: React.FC<TabViewProps> = ({ data }) => {
+  return (
+    <div className="p-4 bg-white rounded-lg border border-gray-300">
+      <div className="text-xl font-bold text-gray-800">
+        {data?.DisplayName || ""}
+      </div>
+    </div>
+  );
+};
+
+export default TabView;
